c087a123e213f0b6527260ee64ad62a1f0f80884
Ayako Prokopczuk <ayprokop@c2r12s1.42wolfsburg.de>
2024-06-13 11:29:36 +0200

    push_swap.c

---
+++ push_swap.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayprokop <ayprokop@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/11 12:41:33 by ayprokop          #+#    #+#             */
+/*   Updated: 2024/06/12 09:43:54 by ayprokop         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("%lu\n", sizeof(long));
+	// how many arguments are there?
+	// If no parameters are specified, the program must not display anything and give the
+	// prompt back.
+	// if (argc )
+
+	// if error
+	// 	"ERROR\n"
+
+	// are the arguments numbers?
+
+	// which sorting is suitable?
+
+	// sorting (using instructions)cd
+	
+
+	// output
+	// instructions must be separated by a ’\n’ 
+	return (0);
+}
